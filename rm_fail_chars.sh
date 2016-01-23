fa376fceb69581fdd2905f0e234a25770e66406e
Yiling Cao <yiling.cao@gmail.com>
2016-01-24 01:19:19 +0800

    add chars failed samples.

--- rm_fail_chars.sh
+++ rm_fail_chars.sh
@@ -14,4 +14,25 @@ rm `find . -name  "*_163_*"`
 rm `find . -name  "*_164_*"`
 rm `find . -name  "*_165_*"`
 
+#failed chars
 
+rm `find . -name  "*_0_*"`
+rm `find . -name  "*_1_*"`
+rm `find . -name  "*_26_*"`
+rm `find . -name  "*_42_*"`
+rm `find . -name  "*_50_*"`
+rm `find . -name  "*_54_*"`
+rm `find . -name  "*_55_*"`
+rm `find . -name  "*_57_*"`
+rm `find . -name  "*_58_*"`
+rm `find . -name  "*_66_*"`
+rm `find . -name  "*_70_*"`
+rm `find . -name  "*_80_*"`
+rm `find . -name  "*_101_*"`
+rm `find . -name  "*_102_*"`
+rm `find . -name  "*_103_*"`
+rm `find . -name  "*_104_*"`
+rm `find . -name  "*_111_*"`
+rm `find . -name  "*_126_*"`
+rm `find . -name  "*_127_*"`
+rm `find . -name  "*_161_*"`
