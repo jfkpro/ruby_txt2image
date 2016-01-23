bc4ff3d8ff823179c8c6ee08aaea4d994f9e23e4
Yiling Cao <yiling.cao@gmail.com>
2016-01-24 00:42:10 +0800

    add script to remove failed english chars.

---
+++ rm_fail_chars.sh
@@ -0,0 +1,17 @@
+rm `find . -name  "*_2_*"`
+rm `find . -name  "*_21_*"`
+rm `find . -name  "*_31_*"`
+rm `find . -name  "*_47_*"`
+rm `find . -name  "*_48_*"`
+rm `find . -name  "*_75_*"`
+rm `find . -name  "*_81_*"`
+rm `find . -name  "*_89_*"`
+rm `find . -name  "*_91_*"`
+rm `find . -name  "*_107_*"`
+rm `find . -name  "*_112_*"`
+rm `find . -name  "*_162_*"`
+rm `find . -name  "*_163_*"`
+rm `find . -name  "*_164_*"`
+rm `find . -name  "*_165_*"`
+
+
